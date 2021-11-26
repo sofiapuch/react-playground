53139179191773590fde71e1a146e2f2a875b91d
Sofia Puch <sofia.puch@aperto.com>
2021-11-26 14:37:41 +0100

    Add colour variables and update navigation

--- src/layout/navigation/Navigation.js
+++ src/layout/navigation/Navigation.js
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom';
 
+import './Navigation.scss';
+
 const Navigation = () => {
   return (
     <div className="navigation">
-      <nav>
-        <Link to="/expenses">Expenses</Link>
-        <Link to="/user-component">User Component</Link>
-      </nav>
+      <div className="grid-container">
+        <nav className="navigation__main">
+          <Link className="navigation__link" to="/expenses">Expenses</Link>
+          <Link className="navigation__link" to="/user-component">User Component</Link>
+        </nav>
+      </div>
     </div>
   )
 }
